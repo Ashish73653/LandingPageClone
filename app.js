25fbbd8a4bacdcd058fa8d2b24c1888c1400f33a
Ashish singh <as4797singh@gmail.com>
2021-07-12 19:16:45 +0530

    [Finishd for small screens]

---
+++ app.js
@@ -0,0 +1,11 @@
+function myFunction(){
+    var x = document.getElementById("menu"); 
+     
+     if(x.style.display == "none")
+     {
+         x.style.display = "flex";
+     }
+     else{
+         x.style.display = "none";
+     }
+ }
